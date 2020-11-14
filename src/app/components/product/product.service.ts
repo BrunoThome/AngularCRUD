7914cbde261bb33e938fe8e4c012a9190d2f0a42
Bruno Thomé <brunothomee@hotmail.com>
2020-11-13 23:30:19 -0300

    feat(basic-version): finalizado os 4 verbos http principais

---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  baseUrl = 'http://localhost:3001/products';
+
+  constructor(private snackBar: MatSnackBar, private httpClient: HttpClient) {}
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
+    });
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e)),
+    );
+  }
+
+  read(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e)),
+    );
+  }
+
+  readById(id: number): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.baseUrl}/${id}`).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e)),
+    );
+  }
+
+  update(product: Product): Observable<Product> {
+    return this.httpClient
+      .put<Product>(`${this.baseUrl}/${product.id}`, product)
+      .pipe(
+        map((obj) => obj),
+        catchError((e) => this.errorHandler(e)),
+      );
+  }
+
+  delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.baseUrl}/${id}`).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e)),
+    );
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+}
